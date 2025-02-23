922c6f9f49413ba07710dadd9f19dc97ae22be0e
ExoticDG <110739962+ExoticDG@users.noreply.github.com>
2025-02-23 17:02:27 -0600

    ALPHA update #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
@@ -12,8 +13,8 @@ android {
         applicationId = "com.exoticdg.weatheralerts"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 0
+        versionName = "ALPHA"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -53,7 +54,10 @@ dependencies {
     implementation(libs.navigation.ui.ktx)
     implementation(libs.androidx.preference)
     implementation(libs.play.services.maps)
+    implementation(libs.androidx.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
 }
